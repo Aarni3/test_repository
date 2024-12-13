6555df828168fa11410f7011dc5e02653c6ad623
Aarni <aarni.eijs@edu.turkuamk.fi>
2024-12-13 20:05:46 +0200

    salasanan murtaja 1.0

--- testi.py
+++ testi.py
@@ -1,1 +1,7 @@
-print("Tämä on Git harjoitus")
+salasana = int(input("Anna salasana 5 numeroinen salasana: "))
+
+print("Salasanan etsintä käynnissä...")
+
+for luku in range(0, 100000):
+    if luku == salasana:
+        print(f"Salasanasi on {luku}")
